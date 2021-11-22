044c7d64aca36cf667fb650e138d24afca85a8a9
Maxnarelf <maxnarelf@gmail.com>
2021-11-22 20:58:24 +0200

    update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,28 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
-  StyleSheet, View, TouchableOpacity, FlatList, Image
+  StyleSheet, View, TouchableOpacity, Image
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import ProfileSvg from '../../assets/icons/ProfileSvg.svg';
+import SvgProfile from '../../assets/icons/user-circle-solid.svg';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'row',
-    // justifyContent: 'space-between',
+    justifyContent: 'space-between',
     alignItems: 'flex-end',
     marginBottom: 30,
-    borderWidth: 4,
-    borderColor: 'yellow'
+    // borderWidth: 4,
+    // borderColor: 'yellow'
   },
 
   item: {
-    width: '100%',
-    borderWidth: 1,
-    borderRadius: 160,
+    width: 40,
+    borderRadius: 40,
   },
   itemMenu: {
-    width: 80,
+    width: 40,
     borderWidth: 1,
     borderColor: 'pink',
     borderRadius: 80,
@@ -41,52 +40,38 @@ const styles = StyleSheet.create({
     color: '#474747'
   },
   img: {
-    width: 80,
-    height: 80,
-    borderRadius: 80,
+    width: 40,
+    height: 40,
+    borderRadius: 40,
     // paddingHorizontal: 10,
 
   }
 });
 const Home = () => {
   const navigation = useNavigation();
-  const [news] = useState([
-    {
-      name: 'Google',
-      anons: 'Google!!!',
-      full: 'Google is cool',
-      key: '1',
-      img: 'https://i.pinimg.com/736x/cc/12/f0/cc12f022a84e9fc25cd7facdb0749ae1.jpg'
-    }
-  ]);
+
   return (
     <View style={styles.container}>
-      {/* <IconSvgProfile /> */}
-
-      <FlatList
-        data={news}
-        renderItem={({ item }) => (
-          <TouchableOpacity style={styles.item} onPress={() => navigation.navigate('Profile', item)}>
-            <Image style={styles.img} source={{ uri: item.img }} />
-
-          </TouchableOpacity>
-
-        )}
-      />
-      <TouchableOpacity style={styles.itemMenu} onPress={() => navigation.navigate('Profile', news)}>
+      <SvgProfile width={60} height={60} />
+      <TouchableOpacity style={styles.item} onPress={() => navigation.navigate('Profile')}>
+        <Image style={styles.img} source={{ uri: 'https://pics.freeicons.io/uploads/icons/png/5441156781530273523-512.png' }} />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.item} onPress={() => navigation.navigate('Profile')}>
+        <Image style={styles.img} source={{ uri: 'https://pics.freeicons.io/uploads/icons/png/13416400251535694869-512.png' }} />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.item} onPress={() => navigation.navigate('Profile')}>
+        <Image style={styles.img} source={{ uri: 'https://pics.freeicons.io/uploads/icons/png/117199341543238913-512.png' }} />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.item} onPress={() => navigation.navigate('Profile')}>
+        <Image style={styles.img} source={{ uri: 'https://pics.freeicons.io/uploads/icons/png/3895539931556258249-512.png' }} />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.itemMenu} onPress={() => navigation.navigate('Profile')}>
         <Image
           style={styles.img}
           source={{ uri: 'https://st3.depositphotos.com/15648834/17930/v/1600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg' }}
         />
       </TouchableOpacity>
 
-      <FlatList
-        data={news}
-        renderItem={({ item }) => (
-          <TouchableOpacity style={styles.itemMenu} onPress={() => navigation.navigate('Profile', item)} />
-        )}
-      />
-      <ProfileSvg width={60} height={60} />
     </View>
 
   );
