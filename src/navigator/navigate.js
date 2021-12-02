1cb70821ca3afad8d4fcaa6396f8c207c913c143
Maxnarelf <maxnarelf@gmail.com>
2021-12-02 23:19:50 +0200

    update

--- src/navigator/navigate.js
+++ src/navigator/navigate.js
@@ -4,47 +4,55 @@ import { NavigationContainer } from '@react-navigation/native';
 // import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 // import PropTypes from 'prop-types';
 // import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
-// import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import Home from '../screens/Home/Home';
 import Profile from '../screens/Profile/Profile';
 import Search from '../screens/Search/Search';
 import Favorite from '../screens/Favorite/Favorite';
 import List from '../screens/List/List';
-// import BottomNavigation from './components/bottomNavigation';
+import BottomNavigation from './components/bottomNavigation';
 // import DrawerNavigation from './components/drawerNavigation';
 
 // const Tab = createBottomTabNavigator();
-// const Tab = createMaterialBottomTabNavigator();
-const Drawer = createDrawerNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 const Navigate = () => {
   return (
     <NavigationContainer>
       {/* <DrawerNavigation /> */}
-      <Drawer.Navigator>
-        <Drawer.Screen
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          header: () => null,
+          tabBarIcon: ({ focused, size, color }) => (
+            <BottomNavigation route={route} focused={focused} size={size} color={color} />
+          )
+        })}
+        activeColor="goldenrod"
+        inactiveColor="#815e08"
+        barStyle={{ backgroundColor: '#303f52' }}
+      >
+        <Tab.Screen
           name="Home"
           component={Home}
         />
-        <Drawer.Screen
+        <Tab.Screen
           name="List"
           component={List}
         />
-        <Drawer.Screen
+        <Tab.Screen
           name="Search"
           component={Search}
         />
-        <Drawer.Screen
+        <Tab.Screen
           name="Favorite"
           component={Favorite}
         />
-        <Drawer.Screen
+        <Tab.Screen
           name="Profile"
           component={Profile}
         />
 
-      </Drawer.Navigator>
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
